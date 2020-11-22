4c8eb53f0d53bbee63001f49bdf9d6fde09efdef
HarshChoudhary123 <73883170+HarshChoudhary123@users.noreply.github.com>
2020-11-22 21:52:48 +0530

    Add files via upload

---
+++ timerCount.vue
@@ -0,0 +1,35 @@
+<template>
+<div style="    margin-top: 20%;
+    font-size: 4rem;
+    font-family: Segoe Print" >
+    <div :style="mycolor">
+    <center>
+    ${{ timerCount }}
+    </center>
+</div>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+        timerCount: 1876548548680,
+        mycolor: 'color: #'+(Math.random()*0xFFFFFF<<0).toString(16)
+    }
+  },
+  watch: {
+    timerCount: {
+        handler(value) {
+            if (value > 0) {
+                setTimeout(() => {
+                    this.timerCount = this.timerCount - 37845763;
+                    this.mycolor = 'color: #'+(Math.random()*0xFFFFFF<<0).toString(16);
+                }, 1);
+            }
+        },
+    immediate: true // This ensures the watcher is triggered upon creation
+    }
+  }
+}
+
+</script>
