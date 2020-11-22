250e443682a8574e26ac4497ad1da1f958ea09f1
HarshChoudhary123 <73883170+HarshChoudhary123@users.noreply.github.com>
2020-11-22 22:04:01 +0530

    Add files via upload

---
+++ timerCount.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    {{ timerCount }}
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+        timerCount: 10,
+    }
+  },
+  watch: {
+    timerCount: {
+        handler(value) {
+            if (value > 0) {
+                setTimeout(() => {
+                    this.timerCount--;
+                }, 10);
+            }
+        },
+    immediate: true // This ensures the watcher is triggered upon creation
+    }
+  }
+}
+
+</script>
